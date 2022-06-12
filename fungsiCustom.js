81819ce115b9e02f06ba5dbfbca5ba2e4413bafd
djindri <indri.djon.hansemit@gmail.com>
2022-06-12 10:24:37 +0700

    baca data dari file json

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+//const bacaData = null;
+
+//baca data JSON
+const bacaData = (fnCallback) => {
+  const ambilKata = [];
+
+  //ambil kata pada file data1.json
+  fs.readFile(file1, (err, data) => {
+      if (err) fnCallback(err);
+      let ambilData = JSON.parse(data);
+      let ambilPesan = ambilData.message.split(" ");
+      ambilKata.push(ambilPesan[1]);
+
+      //ambil kata pada file data2.json
+      fs.readFile(file2, (err, data) => {
+          if (err) fnCallback(err);
+          let ambilData = JSON.parse(data);
+          let ambilPesan = ambilData[0].message.split(" ");
+          ambilKata.push(ambilPesan[1]);
+ 
+          //ambil kata pada file data3.json    
+          fs.readFile(file3, (err, data) => {
+              if (err) fnCallback(err);
+              let ambilData = JSON.parse(data);
+              let ambilPesan = ambilData[0].data.message.split(" ");
+              ambilKata.push(ambilPesan[1]);
+ 
+              return fnCallback(err, ambilKata);
+          });
+      });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
